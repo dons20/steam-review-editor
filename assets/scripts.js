25365690155b87122cb2706c6da4524ac58d8d62
Keno Clayton <keno.clayton15@gmail.com>
2017-05-06 06:27:26 -0500

    More changes

--- assets/scripts.js
+++ assets/scripts.js
@@ -122,6 +122,38 @@ $('#closeBtn').click(function() {
     });
 });
 
+// Store accumulated changes
+var change = new Delta();
+quill.on('text-change', function(delta) {
+  change = change.compose(delta);
+});
+
+// Save periodically
+setInterval(function() {
+  if (change.length() > 0) {
+    console.log('Saving changes', change);
+    /* 
+    Send partial changes
+    $.post('/your-endpoint', { 
+      partial: JSON.stringify(change) 
+    });
+    
+    Send entire document
+    $.post('/your-endpoint', { 
+      doc: JSON.stringify(quill.getContents())
+    });
+    */
+    change = new Delta();
+  }
+}, 5*1000);
+
+// Check for unsaved data
+window.onbeforeunload = function() {
+  if (change.length() > 0) {
+    return 'There are unsaved changes. Are you sure you want to leave?';
+  }
+}
+
 $(document).ready(function () {
     pushFooter();
     var fade = $(".fade");
