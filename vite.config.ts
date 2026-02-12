321323eabeaf8cd0293f6cc8b0770aa6c698db12
Keno Clayton <keno.clayton15@gmail.com>
2026-02-11 19:00:42 -0500

    Updated package dependencies Updated import path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  base: "",
-  plugins: [react(), viteTsconfigPaths()],
-  server: {
-    open: true,
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: "",
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+      open: true,
+      port: Number(env.VITE_PORT) || 3000,
+    }
+  };
 });
